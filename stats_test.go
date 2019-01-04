85d75b8e7a8635825a8eb57ace975f957cea3c11
bayugyug <mtejero@artofclick.com>
2019-01-04 13:23:50 +0800

    - Add test func

--- stats_test.go
+++ stats_test.go
@@ -28,3 +28,26 @@ func TestStatsInit(t *testing.T) {
 		return
 	}
 }
+
+func TestStatsWithOption(t *testing.T) {
+
+	q, err := NewQ(WithQLimit(500))
+	if !assert.NoError(t, err, "New should succeed") {
+		return
+	}
+
+	expected := 100
+	i := 0
+	for {
+		i++
+		q.Incr("TEST::STATS")
+		q.FloatIncr("DECIMAL::STATS::TEST")
+		if i >= 100 {
+			break
+		}
+
+	}
+	if !assert.Equal(t, expected, i) {
+		return
+	}
+}
