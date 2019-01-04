62279f6e4adcd785b596dae6dba6b689e4ac8fba
bayugyug <mtejero@artofclick.com>
2019-01-04 15:27:47 +0800

    - Tweak more ...

--- stats.go
+++ stats.go
@@ -12,17 +12,17 @@ import (
 const QLimit = 20000
 
 type QVal struct {
-	Value   string  `json:",omitempty"`
-	ByInt   int     `json:",omitempty"`
-	ByFloat float64 `json:",omitempty"`
+	value   string  `json:",omitempty"`
+	byInt   int     `json:",omitempty"`
+	byFloat float64 `json:",omitempty"`
 }
 
 type Stats struct {
-	QInt     map[string]int     `json:",omitempty"`
-	QFloat   map[string]float64 `json:",omitempty"`
-	Modified string             `json:",omitempty"`
-	Queue    chan QVal          `json:"-"`
-	Lock     sync.Mutex         `json:"-"`
+	qInts    map[string]int     `json:",omitempty"`
+	qFloats  map[string]float64 `json:",omitempty"`
+	modified string             `json:",omitempty"`
+	qRows    chan QVal          `json:"-"`
+	lock     sync.Mutex         `json:"-"`
 }
 
 // Option is used to pass optional arguments to
@@ -45,7 +45,7 @@ func WithQLimit(m int) Option {
 	return OptionCallback(func(q *Stats) error {
 		//just in case
 		if m > 0 {
-			q.Queue = make(chan QVal, m)
+			q.qRows = make(chan QVal, m)
 		}
 		return nil
 	})
@@ -57,10 +57,10 @@ func NewQ(opts ...Option) (*Stats, error) {
 
 	//init defaults here
 	q := &Stats{
-		Modified: time.Now().Format("2006-01-02 15:04:05.999"),
-		Queue:    make(chan QVal, QLimit),
-		QInt:     make(map[string]int),
-		QFloat:   make(map[string]float64),
+		modified: time.Now().Format("2006-01-02 15:04:05.999"),
+		qRows:    make(chan QVal, QLimit),
+		qInts:    make(map[string]int),
+		qFloats:  make(map[string]float64),
 	}
 
 	//add options if any
@@ -80,13 +80,13 @@ func NewQ(opts ...Option) (*Stats, error) {
 //Reload set to initial state
 func (q *Stats) Reload() {
 	//init again
-	q.Lock.Lock()
-	defer q.Lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
 	//do the reset
-	q.Modified = time.Now().Format("2006-01-02 15:04:05.999")
-	q.QInt = make(map[string]int)
-	q.QFloat = make(map[string]float64)
+	q.modified = time.Now().Format("2006-01-02 15:04:05.999")
+	q.qInts = make(map[string]int)
+	q.qFloats = make(map[string]float64)
 
 	return
 }
@@ -94,15 +94,15 @@ func (q *Stats) Reload() {
 //Stringify
 func (q *Stats) Stringify() string {
 	//init again
-	q.Lock.Lock()
-	defer q.Lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
 	//format it
 	s := ""
-	for k, v := range q.QInt {
+	for k, v := range q.qInts {
 		s += fmt.Sprintf("%-20s => %d\n", k, v)
 	}
-	for k, v := range q.QFloat {
+	for k, v := range q.qFloats {
 		s += fmt.Sprintf("%-20s => %.08f\n", k, v)
 	}
 	return strings.TrimSpace(s)
@@ -111,16 +111,16 @@ func (q *Stats) Stringify() string {
 //JSONify
 func (q *Stats) JSONify() string {
 	//init again
-	q.Lock.Lock()
-	defer q.Lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
 	//format it
 	var j1, j2 []byte
-	if len(q.QInt) > 0 {
-		j1, _ = json.MarshalIndent(q.QInt, "", "\t")
+	if len(q.qInts) > 0 {
+		j1, _ = json.MarshalIndent(q.qInts, "", "\t")
 	}
-	if len(q.QFloat) > 0 {
-		j2, _ = json.MarshalIndent(q.QFloat, "", "\t")
+	if len(q.qFloats) > 0 {
+		j2, _ = json.MarshalIndent(q.qFloats, "", "\t")
 	}
 	return strings.TrimSpace(fmt.Sprintf("%s\n%s", string(j1), string(j2)))
 }
@@ -128,49 +128,49 @@ func (q *Stats) JSONify() string {
 //Incr stats by int:1
 func (q *Stats) Incr(v string) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByInt: 1}
+	q.qRows <- QVal{value: v, byInt: 1}
 }
 
 //IncrBy stats by int:1
 func (q *Stats) IncrBy(v string, t int) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByInt: t}
+	q.qRows <- QVal{value: v, byInt: t}
 }
 
 //Decr stats by int: -1
 func (q *Stats) Decr(v string) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByInt: -1}
+	q.qRows <- QVal{value: v, byInt: -1}
 }
 
 //Decr stats by int:1
 func (q *Stats) DecrBy(v string, t int) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByInt: -t}
+	q.qRows <- QVal{value: v, byInt: -t}
 }
 
 //FloatIncr stats by decimal:1.0
 func (q *Stats) FloatIncr(v string) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByFloat: 1.0}
+	q.qRows <- QVal{value: v, byFloat: 1.0}
 }
 
 //FloatIncrBy stats by decimal:n.nn
 func (q *Stats) FloatIncrBy(v string, t float64) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByFloat: t * 1.0}
+	q.qRows <- QVal{value: v, byFloat: t * 1.0}
 }
 
 //FloatDecr stats by decimal: -1.0
 func (q *Stats) FloatDecr(v string) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByFloat: -1.0}
+	q.qRows <- QVal{value: v, byFloat: -1.0}
 }
 
 //FloatDecr stats by decimal: -n.nn
 func (q *Stats) FloatDecrBy(v string, t float64) {
 	//dont worry about racing :-)
-	q.Queue <- QVal{Value: v, ByFloat: -t * 1.0}
+	q.qRows <- QVal{value: v, byFloat: -t * 1.0}
 }
 
 //Watch monitor the queue data
@@ -178,12 +178,12 @@ func (q *Stats) Watch(isReady chan bool) {
 	isReady <- true
 	for {
 		select {
-		case m := <-q.Queue:
-			if m.ByInt != 0 {
-				q.QInt[m.Value] += m.ByInt
+		case m := <-q.qRows:
+			if m.byInt != 0 {
+				q.qInts[m.value] += m.byInt
 			}
-			if m.ByFloat != 0 {
-				q.QFloat[m.Value] += m.ByFloat
+			if m.byFloat != 0 {
+				q.qFloats[m.value] += m.byFloat
 			}
 		case <-time.After(1 * time.Nanosecond):
 		}
@@ -192,20 +192,20 @@ func (q *Stats) Watch(isReady chan bool) {
 
 //Dump print all available stats
 func (q *Stats) Dump() {
-	fmt.Println(fmt.Sprintf("%-20s => %s\n%s", "Modified", q.Modified, q.SortIt()))
+	fmt.Println(fmt.Sprintf("%-20s => %s\n%s", "Modified", q.modified, q.SortIt()))
 }
 
 //SortIt sort the key=value pair
 func (q *Stats) SortIt() string {
 	//init again
-	q.Lock.Lock()
-	defer q.Lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
 	var strs, fmtd []string
 
 	//fmt here
-	q1 := q.QInt
-	q2 := q.QFloat
+	q1 := q.qInts
+	q2 := q.qFloats
 
 	//dump stats::ints
 	for k, _ := range q1 {
@@ -233,9 +233,9 @@ func (q *Stats) SortIt() string {
 //Raw share the actual raw data
 func (q *Stats) Raw() (map[string]int, map[string]float64) {
 	//init again
-	q.Lock.Lock()
-	defer q.Lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
 	//fmt here
-	return q.QInt, q.QFloat
+	return q.qInts, q.qFloats
 }
